fc361cc68e6b448e5c5fd0bc10e803bec26aa37a
Atharv-gif <atharv.giri999@gmail.com>
2025-03-09 02:04:07 +0530

    Create mian.dart
    a

---
+++ mian.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'phoneauth.dart'; // Import Phoneauth screen
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home:  Phoneauth(),
+    );
+  }
+}
